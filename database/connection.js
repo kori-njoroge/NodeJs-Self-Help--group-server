9a11c018d63a3e67019fac8243e653a9c856e0d4
kori-njoroge <korinjoroge63@gmail.com>
2022-11-22 15:23:15 +0300

    more changes db

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('myproject','corey','Kheriac@1',{dialect:'mysql',host:"54.164.66.87"});
+const sequelize = new Sequelize('myproject','corey','Kheriac1@',{dialect:'mysql',host:"54.164.66.87"});
 
 module.exports = sequelize;
